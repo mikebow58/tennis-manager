976fe1d38f711eb149d3b2797a28a249dc79499a
mikebow58 <mikelbowman@gmail.com>
2026-05-10 10:15:18 -0600

    trigger rebuild for env var update

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ export async function middleware(request) {
   return await updateSession(request)
 }
 
+
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|api/cron|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
